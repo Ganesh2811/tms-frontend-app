7ec84f0389e74764fc30b03f24dd2f56dae51650
Ganesh2811 <gp11282001@gmail.com>
2026-03-01 04:14:15 +0530

    login page is done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,54 @@
-import Login from "./pages/Login"
+import { Route, Routes, Navigate, Outlet, useLocation, replace } from "react-router-dom";
+import { Toaster } from "sonner";
+import Dashboard from "./Pages/Dashboard";
+import Tasks from "./Pages/Tasks";
+import Users from "./Pages/Users";
+import Trash from "./Pages/Trash";
+import TaskDetails from "./Pages/TaskDetails";
+import Login from './Pages/Login';
 
+
+function Layout() {
+  const user = "";
+  const location = useLocation();
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <SideBar /> */}
+      </div>
+        {/* <MobileSideBar /> */}
+        <div className="flex-1 overflow-y-auto">
+          <Navbar />
+          <div className="p-4 2x1:px-10">
+            <Outlet />
+          </div>
+        </div>
+    </div>
+  ) :
+  (
+      <Navigate to = "/login" state={{ from: location }} replace />
+  )
+}
 
 function App() {
-
   return (
-    <Login />
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+          <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster richColors />
+    </main>
   )
 }
 
