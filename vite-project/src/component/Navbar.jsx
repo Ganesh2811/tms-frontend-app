57412c91a80e727de0fa52bbebdccadddaaa4c19
Ganesh2811 <gp11282001@gmail.com>
2026-03-07 01:37:37 +0530

    task 4 is complete

--- vite-project/src/component/Navbar.jsx
+++ vite-project/src/component/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className='flex justify-between items-center bg-white px-4 py-3 2xl:py-4 sticky z-10 top-0'>
             <div className='flex gap-4'>
                 <button onClick={() => dispatch(setOpenSideBar(true))} className='text-2xl text-gray-500 block md:hidden'>☰</button>
-                <div className='w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]'>
+                <div className='w-64 2xl:w-100 flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]'>
                     <MdOutlineSearch className='text-gray-500 text-xl'/>
                     <input 
                         type="text" 
